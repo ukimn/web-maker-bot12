423e669a276fa73da82be6508897ce2540ce7f94
ukimn <128185440+ukimn@users.noreply.github.com>
2023-07-09 08:03:51 -0700

    Add files via upload

---
+++ web maker bot.py
@@ -0,0 +1,40 @@
+print("welcome to web maker bot")
+info = input("do you need some info (recomanded)?:")
+if info == "yes" or info == "YES":
+    print("okay let's go")
+    print("if you need to put imeg in your web write img")
+    print("if you need to put a vedio write ved")
+    print("if you need to change the color write background color(the color) example background color(pink) or background color(p = pink r = red)")
+    print("if you need to put a topic write tip and write your topic example tip(welcome to my web!)")
+    print("if you need to to write in your web write wrt(the words) example wrt(we have many featurs new!)")
+    print("if you need to end the write end")
+else:
+    print("okay")
+
+choose = "what do you need to put in your web?: "
+if choose == "img" or choose == "IMG":
+    print("")
+
+if choose == "ved" or choose == "VED":
+    print("")
+
+if choose == "background color(black)" or choose == "BACKGROUND COLOR(BLACK)":
+    print("")
+
+if choose == "background color(pink)" or choose == "BACKGROUND COLOR(PINK)":
+    print("")
+
+if choose == "background color(red)" or choose == "BACKGROUND COLOR(RED)":
+    print("")
+
+if choose == "background color(blue)" or choose == "BACKGROUND COLOR(BLUE)":
+    print("")
+
+if choose == "background color(yellow)" or choose == "BACKGROUND COLOR(YELLOW)":
+    print("")
+
+if choose == "background color(brown)" or choose == "BACKGROUND COLOR(BROWN)":
+    print("")
+
+if choose == "background color(purple)" or choose == "BACKGROUND COLOR(PURPLE)" or choose == "background color(p)" or choose == "BACKGROUND COLOR(P)":
+    print("")                                    
